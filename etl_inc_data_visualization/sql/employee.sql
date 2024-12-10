8fb9f439259e0978d3b167e479932083e19a25ea
Mallikarjun8080 <arjunarjuncm8080@gmail.com>
2024-12-10 22:49:22 +0530

    Update employee.sql

--- etl_inc_data_visualization/sql/employee.sql
+++ etl_inc_data_visualization/sql/employee.sql
@@ -1,3 +1,13 @@
-SELECT e.employee_id, e.name, e.department_id, d.department_name
-FROM employee e
-JOIN department d ON e.department_id = d.department_id;
+SELECT
+    emp.name AS employee_name,
+    dept.name AS department_name,
+    emp.salary,
+    mgr.name AS manager_name
+FROM
+    employee emp
+JOIN
+    department dept ON emp.dept_id = dept.dept_id
+JOIN
+    employee mgr ON emp.manager_id = mgr.emp_id
+WHERE
+    emp.salary > 50000;
